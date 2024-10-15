f5c11e14180467374d6d17bdc8cd6eee6b9ff414
Alina Vinogradova <ellafoss@yandex.ru>
2024-10-15 08:20:16 +0300

    Усложненное задание, двигающийся квадрат

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const DomElement = function (selector, width, height, bg, fontSize) {
+const DomElement = function (selector, position, width, height, bg, fontSize) {
     this.selector = selector;
+    this.position = position;
     this.height = height;
     this.width = width;
     this.bg = bg;
@@ -19,17 +20,37 @@ const DomElement = function (selector, width, height, bg, fontSize) {
         }
         elem.innerText = text;
         elem.style.cssText = `
+            position: ${this.position};
+            top:0;
+            left: 0;
             width: ${this.width};
             height: ${this.height};
             font-size: ${this.fontSize}px;
             background: ${this.bg};
         `;
         document.body.append(elem);
+        return elem;
     }
 }
 
-const newDiv = new DomElement(".block", "100px", "100px", "yellow", 14);
-const newP = new DomElement("#paragraph", "max-content", "auto", "green", 24);
+document.addEventListener("DOMContentLoaded", () => {
+    const newDiv = new DomElement(".block", "absolute", "100px", "100px", "yellow", 14);
+    let element = newDiv.createElement("Я двигаюсь, следовательно, я существую!");
 
-newDiv.createElement("Я родился!");
-newP.createElement("Приветик");
+    document.addEventListener('keydown', (event) => {
+        switch (event.key) {
+            case "ArrowLeft":
+                element.style.left = parseInt(element.style.left) - 10 + "px";
+                break;
+            case "ArrowUp":
+                element.style.top = parseInt(element.style.top) - 10 + "px";
+                break;
+            case "ArrowRight":
+                element.style.left = parseInt(element.style.left) + 10 + "px";
+                break;
+            case "ArrowDown":
+                element.style.top = parseInt(element.style.top) + 10 + "px";
+                break;
+        }
+    });
+});
