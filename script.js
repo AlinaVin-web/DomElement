e58cd5164a798d4cdf76a558e225219dbed22aa3
Alina Vinogradova <ellafoss@yandex.ru>
2024-10-15 06:45:06 +0300

    Урок 15. Создание элемента через функцию-конструктор

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const DomElement = function (selector, width, height, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.createElement = function (text) {
+        let elem;
+        if (this.selector[0] === ".") {
+            elem = document.createElement('div');
+            elem.className = this.selector.slice(1);
+
+        }
+        if (this.selector[0] === "#") {
+            elem = document.createElement('p');
+            elem.id = this.selector.slice(1);
+        }
+        elem.innerText = text;
+        elem.style.cssText = `
+            width: ${this.width};
+            height: ${this.height};
+            font-size: ${this.fontSize}px;
+            background: ${this.bg};
+        `;
+        document.body.append(elem);
+    }
+}
+
+const newDiv = new DomElement(".block", "100px", "100px", "yellow", 14);
+const newP = new DomElement("#paragraph", "max-content", "auto", "green", 24);
+
+newDiv.createElement("Я родился!");
+newP.createElement("Приветик");
